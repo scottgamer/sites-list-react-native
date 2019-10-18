85febe4bcade79a531b12ce29a29dd8c811420e3
Richard Muñoz <richard.munoz@crifa.com>
2019-10-18 10:49:06 -0500

    fixed app.js

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import AppNavigator from "./src/AppNavigator";
-import { Provider } from "react-redux";
-import store from "./src/store";
-import { mapping, light as lightTheme } from "@eva-design/eva";
-import { ApplicationProvider } from "react-native-ui-kitten";
+import React from 'react';
+import AppNavigator from './src/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './src/store';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider } from 'react-native-ui-kitten';
 
 export default function App() {
   return (
     <ApplicationProvider mapping={mapping} theme={lightTheme}>
       <Provider store={store}>
-        <AppNavigator />
+          <AppNavigator />
       </Provider>
     </ApplicationProvider>
   );
